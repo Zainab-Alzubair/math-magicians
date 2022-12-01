7448abd379e9591e2de23ad42e4abd256f8377c2
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-12-01 20:19:00 +0300

    Add calc logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,93 +1,112 @@
 import React from 'react';
 import '../App.css';
+import calculate from '../logic/calculate'
+
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.eventHandler = this.eventHandler.bind(this);
+  }
+
+  eventHandler(e) {
+    this.setState(calculate(this.state, e.target.value));
+  }
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <table id="calcu">
-        <tr>
-          <td colSpan="4"><input type="text" id="result" value="0" /></td>
-        </tr>
-        <tr>
-          <td>
-            <input type="button" value="AC" className="calc-button" />
-            {' '}
-          </td>
-          <td>
-            <input type="button" value="+/-" className="calc-button" />
-            {' '}
-          </td>
-          <td>
-            <input type="button" value="%" className="calc-button" />
-            {' '}
-          </td>
-          <td>
-            <input type="button" value="+" className="calc-orange-button" />
-            {' '}
-          </td>
-        </tr>
+      <tr>
+        <td colSpan="4"id="result">
+        <p>{total}{operation}{next}</p>
+        </td>
+      </tr>
         <tr>
           <td>
-            <input type="button" value="7" className="calc-button" />
-            {' '}
+            <input type="button" value="AC" className="calc-button" onClick={this.eventHandler}/>
+            
           </td>
           <td>
-            <input type="button" value="8" className="calc-button" />
-            {' '}
+            <input type="button" value="+/-" className="calc-button" onClick={this.eventHandler}/>
+           
           </td>
           <td>
-            <input type="button" value="9" className="calc-button" />
-            {' '}
+            <input type="button" value="%" className="calc-button"onClick={this.eventHandler} />
+           
           </td>
           <td>
-            <input type="button" value="*" className="calc-orange-button" />
-            {' '}
+            <input type="button" value="+" className="calc-orange-button"onClick={this.eventHandler} />
+       
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="4" className="calc-button" />
-            {' '}
+            <input type="button" value="7" className="calc-button" onClick={this.eventHandler}/>
+         
           </td>
           <td>
-            <input type="button" value="5" className="calc-button" />
-            {' '}
+            <input type="button" value="8" className="calc-button" onClick={this.eventHandler}/>
+      
           </td>
           <td>
-            <input type="button" value="6" className="calc-button" />
-            {' '}
+            <input type="button" value="9" className="calc-button" onClick={this.eventHandler}/>
+      
           </td>
           <td>
-            <input type="button" value="-" className="calc-orange-button" />
-            {' '}
+            <input type="button" value="*" className="calc-orange-button"onClick={this.eventHandler} />
+        
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="1" className="calc-button" />
-            {' '}
+            <input type="button" value="4" className="calc-button" onClick={this.eventHandler}/>
+      
           </td>
           <td>
-            <input type="button" value="2" className="calc-button" />
-            {' '}
+            <input type="button" value="5" className="calc-button" onClick={this.eventHandler}/>
+     
           </td>
           <td>
-            <input type="button" value="3" className="calc-button" />
-            {' '}
+            <input type="button" value="6" className="calc-button"onClick={this.eventHandler} />
+    
           </td>
           <td>
-            <input type="button" value="+" className="calc-orange-button" />
-            {' '}
+            <input type="button" value="-" className="calc-orange-button"onClick={this.eventHandler} />
+        
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="button" value="1" className="calc-button" onClick={this.eventHandler}/>
+         
+          </td>
+          <td>
+            <input type="button" value="2" className="calc-button" onClick={this.eventHandler}/>
+         
+          </td>
+          <td>
+            <input type="button" value="3" className="calc-button" onClick={this.eventHandler}/>
+         
+          </td>
+          <td>
+            <input type="button" value="+" className="calc-orange-button" onClick={this.eventHandler}/>
+        
           </td>
         </tr>
         <tr>
           <td colSpan="3">
-            <input type="button" value="0" className="zero-btn" />
-            {' '}
+            <input type="button" value="0" className="zero-btn" onClick={this.eventHandler}/>
+       
           </td>
           <td>
-            <input type="button" value="=" className="calc-orange-button" />
-            {' '}
+            <input type="button" value="=" className="calc-orange-button"onClick={this.eventHandler} />
+          
           </td>
         </tr>
       </table>
