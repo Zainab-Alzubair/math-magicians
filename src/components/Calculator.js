8e8be2692938a08f0e1ed428a881f0bc2a0de575
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-12-01 20:51:22 +0300

    Fix linters error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
@@ -11,10 +11,11 @@ class Calculator extends React.Component {
       next: null,
       operation: null,
     };
-    this.eventHandler = this.eventHandler.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  eventHandler(e) {
+  handleClick(e) {
+    // eslint-disable-next-line react/no-access-state-in-setstate
     this.setState(calculate(this.state, e.target.value));
   }
 
@@ -22,91 +23,95 @@ class Calculator extends React.Component {
     const { total, next, operation } = this.state;
     return (
       <table id="calcu">
-      <tr>
-        <td colSpan="4"id="result">
-        <p>{total}{operation}{next}</p>
-        </td>
-      </tr>
         <tr>
-          <td>
-            <input type="button" value="AC" className="calc-button" onClick={this.eventHandler}/>
-            
-          </td>
-          <td>
-            <input type="button" value="+/-" className="calc-button" onClick={this.eventHandler}/>
-           
-          </td>
-          <td>
-            <input type="button" value="%" className="calc-button"onClick={this.eventHandler} />
-           
-          </td>
-          <td>
-            <input type="button" value="+" className="calc-orange-button"onClick={this.eventHandler} />
-       
+          <td colSpan="4" id="result">
+            <p>
+              {total}
+              {operation}
+              {next}
+            </p>
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="7" className="calc-button" onClick={this.eventHandler}/>
-         
+            <input type="button" value="AC" className="calc-button" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="8" className="calc-button" onClick={this.eventHandler}/>
-      
+            <input type="button" value="+/-" className="calc-button" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="9" className="calc-button" onClick={this.eventHandler}/>
-      
+            <input type="button" value="%" className="calc-button" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="*" className="calc-orange-button"onClick={this.eventHandler} />
-        
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <input type="button" value="4" className="calc-button" onClick={this.eventHandler}/>
-      
-          </td>
-          <td>
-            <input type="button" value="5" className="calc-button" onClick={this.eventHandler}/>
-     
-          </td>
-          <td>
-            <input type="button" value="6" className="calc-button"onClick={this.eventHandler} />
-    
-          </td>
-          <td>
-            <input type="button" value="-" className="calc-orange-button"onClick={this.eventHandler} />
-        
+            <input type="button" value="+" className="calc-orange-button" onClick={this.handleClick} />
+
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="1" className="calc-button" onClick={this.eventHandler}/>
-         
+            <input type="button" value="7" className="calc-button" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="2" className="calc-button" onClick={this.eventHandler}/>
-         
+            <input type="button" value="8" className="calc-button" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="3" className="calc-button" onClick={this.eventHandler}/>
-         
+            <input type="button" value="9" className="calc-button" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="+" className="calc-orange-button" onClick={this.eventHandler}/>
-        
+            <input type="button" value="x" className="calc-orange-button" onClick={this.handleClick} />
+
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="button" value="4" className="calc-button" onClick={this.handleClick} />
+
+          </td>
+          <td>
+            <input type="button" value="5" className="calc-button" onClick={this.handleClick} />
+
+          </td>
+          <td>
+            <input type="button" value="6" className="calc-button" onClick={this.handleClick} />
+
+          </td>
+          <td>
+            <input type="button" value="-" className="calc-orange-button" onClick={this.handleClick} />
+
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="button" value="1" className="calc-button" onClick={this.handleClick} />
+
+          </td>
+          <td>
+            <input type="button" value="2" className="calc-button" onClick={this.handleClick} />
+
+          </td>
+          <td>
+            <input type="button" value="3" className="calc-button" onClick={this.handleClick} />
+
+          </td>
+          <td>
+            <input type="button" value="÷" className="calc-orange-button" onClick={this.handleClick} />
+
           </td>
         </tr>
         <tr>
           <td colSpan="3">
-            <input type="button" value="0" className="zero-btn" onClick={this.eventHandler}/>
-       
+            <input type="button" value="0" className="zero-btn" onClick={this.handleClick} />
+
           </td>
           <td>
-            <input type="button" value="=" className="calc-orange-button"onClick={this.eventHandler} />
-          
+            <input type="button" value="=" className="calc-orange-button" onClick={this.handleClick} />
+
           </td>
         </tr>
       </table>
